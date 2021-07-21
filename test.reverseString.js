0c3d3b4fa1921900519eda469cb859a2e8cc27ee
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-07-21 17:43:22 +0100

    feat: Add reverse string test

---
+++ test.reverseString.js
@@ -0,0 +1,13 @@
+const reverseString = require('./reverseString');
+
+test('adds coding to equals gnidoc', () => {
+  expect(strLength('coding')).toBe('gnidoc');
+});
+
+test('adds helper to equals repleh', () => {
+    expect(strLength('helper')).toBe('repleh');
+  });
+
+  test('adds technology to equals ygolonhcet', () => {
+    expect(strLength('technology')).toBe('ygolonhcet');
+  });
